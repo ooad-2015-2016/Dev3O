d16e1152d4bf50f64581ea4502dffdc5aae1326a
Aleksandar Acimovic <acimovic.alek@gmail.com>
2016-05-19 12:41:31 +0200

    Model vol.1.1

--- Projekat/BankSystemFundamentals/BankSystemFundamentals/Models/Employee.cs
+++ Projekat/BankSystemFundamentals/BankSystemFundamentals/Models/Employee.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace BankSystemFundamentals.Models
 {
-    class Employee
+    public class Employee : Models.Account
     {
+        public enum EmployeePositions
+        {
+            Admin,
+            Supervisor,
+            Referent
+        }
+
+        public int EmployeeID { get; set; }
+        public String WorkPlace { get; set; }
+        public EmployeePositions Position { get; set; }
+        public Employee(
+            String name,
+            String surname,
+            String nameOfFather,
+            String adress,
+            String city,
+            int postalNumber,
+            String telephoneNumber,
+            String email,
+            String username,
+            String password,
+            int employeeID,
+            EmployeePositions position
+            ) : base(name, surname, nameOfFather, adress, city, postalNumber, telephoneNumber, email, username, password)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.NameOfFather = nameOfFather;
+            this.Adress = adress;
+            this.City = city;
+            this.PostalNumber = postalNumber;
+            this.TelephoneNumber = telephoneNumber;
+            this.Email = email;
+            this.Username = username;
+            this.Password = password;
+            this.EmployeeID = employeeID;
+            this.Position = position;
+        }
+
     }
 }
--- Projekat/BankSystemFundamentals/BankSystemFundamentals/Models/Person.cs
+++ Projekat/BankSystemFundamentals/BankSystemFundamentals/Models/Person.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace BankSystemFundamentals.Models
 {
-    class Person
+    public abstract class Person
     {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String NameOfFather { get; set; }
+        public String Adress { get; set; }
+        public String City { get; set; }
+        public int PostalNumber { get; set; }
+        public String TelephoneNumber { get; set; }
+        public String Email { get; set; }
     }
 }
